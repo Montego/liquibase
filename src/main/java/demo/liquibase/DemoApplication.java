090affb52f35901b5db5a7609a48dcdf465cc037
Montego <bar_d91@mail.ru>
2023-02-04 21:46:32 +0300

    test

---
+++ src/main/java/demo/liquibase/DemoApplication.java
@@ -0,0 +1,14 @@
+package demo.liquibase;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(demo.liquibase.DemoApplication.class, args);
+	}
+
+}
